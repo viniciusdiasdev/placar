782fee5be4cedd3d50c6cfb7087cc51101072053
Vinicius Dias <viniciusdiasdev@gmail.com>
2022-12-06 09:11:28 -0300

    Implementando alguns testes para erros de HTTP.

--- resultado/src/main/java/com/placar/resultado/controller/RestController.java
+++ resultado/src/main/java/com/placar/resultado/controller/RestController.java
@@ -1,10 +1,10 @@
 package com.placar.resultado.controller;
 
 import com.placar.resultado.model.Placar;
-import com.placar.resultado.repository.PlacarRepository;
 import com.placar.resultado.service.PlacarDao;
 import com.placar.resultado.service.PlacarService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
@@ -12,17 +12,37 @@ import java.util.Map;
 
 @org.springframework.web.bind.annotation.RestController
 @RequestMapping("placar")
-public class RestController {
+public class    RestController {
     @Autowired
     private PlacarService placarService;
 
     @PostMapping
-    public ResponseEntity createPlacar(@RequestBody Map<String, Integer> json){
+    public ResponseEntity<PlacarDao> createPlacar(@RequestBody Map<String, Integer> json){
         Integer timeA = json.get("TimeA");
         Integer timeB = json.get("TimeB");
         PlacarDao placarDao = new PlacarDao(timeA, timeB);
         placarService.createPlacar(placarDao);
-        return ResponseEntity.ok("Placar registrado com sucesso");
+        return new ResponseEntity<>(placarDao, HttpStatus.OK);
+    }
+
+    @PostMapping("/notfound")
+    @ResponseStatus(HttpStatus.NOT_FOUND)
+    public void notFound(@RequestBody Map<String, Integer> json){
+    }
+
+    @PostMapping("/badrequest")
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    public void badRequest(@RequestBody Map<String, Integer> json){
+    }
+
+    @PostMapping("/badgateway")
+    @ResponseStatus(HttpStatus.BAD_GATEWAY)
+    public void badGateway(@RequestBody Map<String, Integer> json){
+    }
+
+    @PostMapping("/internal")
+    @ResponseStatus(HttpStatus.INTERNAL_SERVER_ERROR)
+    public void internalError(@RequestBody Map<String, Integer> json){
     }
 
     @GetMapping("/{id}")
